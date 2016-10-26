e89e31c5c22bfc25525f07201905079fe9960898
Arthur Scharf <arthur.scharf@gmail.com>
2016-10-27 01:03:28 +0200

    implemented gaussian 2D fit

--- plotspec.py
+++ plotspec.py
@@ -7,10 +7,49 @@
 
 import json
 import numpy
+import copy
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from numpy import array
-from scipy.interpolate import interp2d
+import scipy.optimize as scp
+
+
+# we use two different functions for our gaussian fit process
+# gaussian2D is used to create a gaussian2D curve with given Parameters
+# gaussian2D_CurveFit is used for the curve fitting process (we need to use ravel here)
+
+
+# gives a gaussion2D for given Parameters
+# see here: https://en.wikipedia.org/wiki/Gaussian_function#Meaning_of_parameters_for_the_general_equation
+# we use lambda function for az0 and el0
+def gaussian2D(offAz, offEl, majWidth, minWidth, thetaRot, offsetZero, amp):
+    # calculate the general gaussian parameters
+    # to check: this could be probably done also by just calculating the mean vector
+    # and 0.5/covariance_matrix with given data... ?
+    a = (numpy.cos(thetaRot)**2)/(2*majWidth**2) + (numpy.sin(thetaRot)**2)/(2*minWidth**2)
+    b = -(numpy.sin(2*thetaRot))/(4*majWidth**2) + (numpy.sin(2*thetaRot))/(4*minWidth**2)
+    c = (numpy.sin(thetaRot)**2)/(2*majWidth**2) + (numpy.cos(thetaRot)**2)/(2*minWidth**2)
+    return lambda az0,el0 : (offsetZero + amp*numpy.exp( - (a*((offAz-az0)**2) + 2*b*(offAz-az0)*(offEl-el0) + c*((offEl-el0)**2))))
+
+
+# used for fitting the curve with curve_fit and leastsq - that's why we have to give the el/az data!
+def gaussian2D_CurveFit(data,offAz, offEl, majWidth, minWidth, thetaRot, offsetZero, amp):
+    az0,el0 = data
+    a = (numpy.cos(thetaRot)**2)/(2*majWidth**2) + (numpy.sin(thetaRot)**2)/(2*minWidth**2)
+    b = -(numpy.sin(2*thetaRot))/(4*majWidth**2) + (numpy.sin(2*thetaRot))/(4*minWidth**2)
+    c = (numpy.sin(thetaRot)**2)/(2*majWidth**2) + (numpy.cos(thetaRot)**2)/(2*minWidth**2)
+    ret = offsetZero + amp*numpy.exp( - (a*((offAz-az0)**2) + 2*b*(offAz-az0)*(offEl-el0) + c*((offEl-el0)**2)))
+    return ret.ravel()
+
+
+# adjusts the zerolevel to the smallest measurement, then normalizes wrt the maximum
+def normalizeAndMove(z):
+    res = z
+    resMin = z.min()
+    res -= resMin
+    resMax = res.max()
+    res /= resMax
+    return res
 
 
 
@@ -91,19 +130,45 @@ print len(zi)
 X,Y = numpy.meshgrid(xi,yi)
 Z=zi.reshape(len(yi),len(xi))
 
+# normalizes and accounts for zero offset (so we stretch the data from 0 to 1)
+Z = normalizeAndMove(Z)
+data = (X,Y)
 
+# we need az0 and el0 for later lambda parse
+az0 = copy.deepcopy(X)
+el0 = copy.deepcopy(Y)
+
+# we guess initial parameters, sun should be in the middle, so azOff=elOff=0
+# also I guess the zero offset should be zero...
+paramGuess = [0,0,1,1,1,0,1]
+
+# now we run curve_fit to get the predicted parameters (starting off on our guessed ones)
+# curve_fit uses leastsq internally
+pparam, covmat = scp.curve_fit(gaussian2D_CurveFit,data,numpy.ravel(Z),p0=paramGuess)
+print "predicted params: ",pparam
+print "estimated covariance matrix: ", covmat
+
+# we create the dataset with the predicted parameters
+fitted = gaussian2D(*pparam)(az0,el0)
+peak = [pparam[0],pparam[1]]
+peakAz = pparam[0]
+peakEl = pparam[1]
+print "fitted: ", fitted
+print "peak: ",peak
+
+# plot the stuff now
 plt.clf()
-plt.pcolormesh(X, Y, Z) # here xi and yi are the vectors containing the offsets and zi contains the stream.
+plt.pcolormesh(X, Y, fitted) # here xi and yi are the vectors containing the offsets and fitted contains the fitted data
 #plt.show()
 plt.colorbar()
 
-
-plt.scatter(X, Y, c = Z, s = 100, vmin = Z.min(), vmax = Z.max())
-plt.contour(X, Y, Z, 1000)
+plt.scatter(X, Y, c = fitted, s = 100, vmin = fitted.min(), vmax = fitted.max())
+plt.contour(X, Y, fitted, 900)
 plt.xlabel( 'azimuth')
 plt.ylabel( 'elevation')
 plt.title( 'Solar flow map')
-
+# plot the peak
+plt.plot(peakAz,peakEl,'bx',mew=2,ms=15)
 plt.show()
 # filenametmp=date + ".png"
 # print "backup file", filenametmp
